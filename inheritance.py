88ce04c266c3b3166f2bc055a32bb4fe0a8c7130
hannahc-0720 <kamhchen@gmail.com>
2025-07-03 18:30:34 -0700

    change

--- inheritance.py
+++ inheritance.py
@@ -6,12 +6,35 @@ class Pet():
         self.name = name
         self.age = age
         self.color = color
+    
+    def feed(self, food):
+        print(f"{self.name} eats {food}.")
 
 # Subclass
 
 class Cat(Pet):
-    def __init__(self, name):
-        self.name = name
-        
+    # Inheriting the parent constructor
+    def __init__(self, name, age, color):
+        # super() refers to the class that is the superclass
+        # using the superclass constructor to set up the subclass constructor
+        super().__init__(name, age, color)
+    
+    # Specific to the child (sub) class
     def meow(self):
-        print('Meow!')
+        print(f"{self.name} says: Meow!")
+
+class Dog(Pet):
+
+    def __init__(self, name, age, breed):
+        super().__init__(name, age, breed)
+    
+    def bark(self):
+        print(f"{self.name} says: Ruff!")
+
+cat1 = Cat("Troy", 7, "orange")
+cat1.meow()
+cat1.feed("fish")
+
+dog1 = Dog("Leo", 1, "Dachshund")
+dog1.bark()
+dog1.feed("dog treats")
